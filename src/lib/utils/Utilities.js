37b766c9940d91e615d08f5267d91edeb1030025
Kyle S <sodacovaa@gmail.com>
2026-01-04 20:23:38 -0500

    Move over some utilities from Naga v2

--- src/lib/utils/Utilities.js
+++ src/lib/utils/Utilities.js
@@ -1,4 +1,74 @@
+const DISCORD_EPOCH = 1420070400000;
+
+let flags = {
+    "STAFF": {
+        "friendly": "<:DiscordStaff:1102389679380246538> Discord Employee",
+        "description": "User is a Discord employee.",
+        "shift": 0,
+    },
+    "PARTNER": {
+        "friendly": "<a:discordPartner:1006803535238811658> Discord Partner",
+        "description": "User is a Discord partner.",
+        "shift": 1,
+    },
+    "HYPESQUAD": {
+        "friendly": "<:HypeSquadEvents:1102390600675901530> HypeSquad Events",
+        "description": "User is a HypeSquad Events member.",
+        "shift": 2,
+    },
+    "BUG_HUNTER_LEVEL_1": {
+        "friendly": "<:BugHunterLvl1:1102390616849141762> Bug Hunter Level 1",
+        "description": "User is a Bug Hunter.",
+        "shift": 3,
+    },
+    "HYPESQUAD_ONLINE_HOUSE_1": {
+        "friendly": "<:Bravery:1102389675550838824> HypeSquad Bravery",
+        "description": "User is part of HypeSquad Bravery.",
+        "shift": 6,
+    },
+    "HYPESQUAD_ONLINE_HOUSE_2": {
+        "friendly": "<:Brilliance:1102389676498767894> HypeSquad Brilliance",
+        "description": "User is part of HypeSquad Brilliance.",
+        "shift": 7,
+    },
+    "HYPESQUAD_ONLINE_HOUSE_3": {
+        "friendly": "<:Balance:1102389669943062611> HypeSquad Balance",
+        "description": "User is a part of HypeSquad Balance.",
+        "shift": 8,
+    },
+    "PREMIUM_EARLY_SUPPORTER": {
+        "friendly": "<:EarlySupporter:1102389681024401478> Legacy Nitro Subscriber",
+        "description": "User is an Early Supporter.",
+        "shift": 9,
+    },
+    "BUG_HUNTER_LEVEL_2": {
+        "friendly": "<:BugHunterLvl2:1102390618673647716> Bug Hunter Level 2",
+        "description": "User has the gold Bug Hunter badge.",
+        "shift": 14,
+    },
+    "VERIFIED_DEVELOPER": {
+        "friendly": "<:VerifiedDev:1102389683520016484> Verified Bot Developer",
+        "description": "User is a verified bot developer.",
+        "shift": 17,
+    },
+    "CERTIFIED_MODERATOR": {
+        "friendly": "<:DiscordModProgram:1102389678340067399> Discord Moderator Program Member",
+        "description": "User is a Discord certified moderator alum.",
+        "shift": 18,
+    },
+    "ACTIVE_DEVELOPER": {
+        "friendly": "<:ActiveDev:1102389668848349264> Active Developer",
+        "description": "User is an active developer. https://discord.com/developers/active-developer",
+        "shift": 22,
+    },
+};
+
 class Utilities {
+
+  constructor() {
+    this.invite = this.invite = /^(discord.gg\/|discordapp.com\/invite\/)([a-z0-9]+)$/gi;
+  };
+
   async sendSuccess(channelOrInteraction, content) {
     const embeds = [
       { 
@@ -37,6 +107,10 @@ class Utilities {
     return await channelOrInteraction.send({ embeds: embeds });
   }
 
+  getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+  }
+
   hexToRgb(hex) {
     const num = parseInt(hex.replace('#', ''), 16);
     return [
@@ -58,6 +132,61 @@ class Utilities {
     const num = parseInt(int, 10);
     return `#${num.toString(16).padStart(6, '0')}`;
   }
+
+  /**
+   * Returns a list of Discord user flags in a human readable format
+   * @param {Number} flagNumber 
+   * @returns {Array} An array of flag strings
+   */
+  getFlags(flagNumber) {
+      let results = [];
+
+      for (let i = 0; i <= 64; i++) {
+          const bitwise = 1n << BigInt(i);
+  
+          if (flagNumber & parseInt(bitwise)) {
+              const flag = Object.entries(flags).find((f) => f[1].shift === i)?.[0] || `UNKNOWN_FLAG_${i}`;
+              if (flag !== `UNKNOWN_FLAG_${i}`) results.push(flags[flag].friendly);
+          }
+      }
+  
+      return results || "None";
+  }
+
+  /**
+  * Creates a Promise that resolves after a specified duration.
+  * @param {number} ms How long to wait before resolving (in milliseconds)
+  * @returns {Promise<void>}
+  */
+  delayFor(ms) {
+      return new Promise(resolve => {
+        setTimeout(resolve, ms);
+      });
+  }
+
+  convertSnowflakeToDate(snowflake, epoch = DISCORD_EPOCH) {
+    const milliseconds = BigInt(snowflake) >> 22n
+    return new Date(Number(milliseconds) + epoch)
+  }
+
+  validateSnowflake(snowflake, epoch) {
+    if (!Number.isInteger(+snowflake)) {
+      this.sendError(msg.channel, 'That doesn\'t look like a snowflake. Snowflakes contain only numbers.')
+    }
+  
+    if (snowflake < 4194304) {
+        this.sendError(msg.channel, 'That doesn\'t look like a snowflake. Snowflakes are much larger numbers.')
+    }
+
+    const timestamp = this.convertSnowflakeToDate(snowflake, epoch)
+
+    if (Number.isNaN(timestamp.getTime())) {
+        this.sendError(msg.channel, 'That doesn\'t look like a snowflake. Snowflakes have fewer digits.')
+    }
+
+    return timestamp;
+  }
+
 }
 
 module.exports = { Utilities };
